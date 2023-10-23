42c769b109dd0b4f6c7174591b616938a0909317
scottwesenberg <scowesen@gmail.com>
2023-10-23 19:37:06 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using InTheBag.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InTheBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult IndexViewBag()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Peace");
+            WishList.Add("Health");
+            WishList.Add("Hapiness");
+            ViewBag.WishList = WishList;
+            return View();
+        }
+        public IActionResult IndexViewData()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("Quies");
+            WishList.Add("Salutem");
+            WishList.Add("Beatitudinem");
+            ViewData["WishList"] = WishList;
+            return View();
+        }
+        public IActionResult IndexTempData()
+        {
+            IList<string> WishList = new List<string>();
+            WishList.Add("La Paz");
+            WishList.Add("La Salud");
+            WishList.Add("L:a Felicidad");
+            TempData["WishList"] = WishList;
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
